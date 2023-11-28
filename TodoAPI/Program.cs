f9ad15d9e5a00420bcf48a2596de19d44940a6f3
EmFor2001 <k.hajduk.kh@gmail.com>
2023-11-28 14:34:00 +0100

    corse policy added

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var Configuration = builder.Configuration;
 builder.Services.AddDbContext<AppDbContext>(options =>
